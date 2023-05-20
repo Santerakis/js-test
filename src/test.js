9d873c1e214a59722c4ff0407b09564bad7e63e0
Sergey <santerakis@gmail.com>
2023-05-20 11:28:16 +0300

    closure(zamikanie)

--- src/test.js
+++ src/test.js
@@ -1,10 +1,11 @@
-function customMap(callback, array) {
-    let res = []
-    for (let i = 0; i < array.length; i++) {
-        res.push(callback(array[i]))
+function foo (m) {
+    return (c) => {
+        return c * m
     }
-    return res
 }
 
-console.log('customMap: ', customMap((el) => el + 1, [1, 2, 3]))
-console.log('map: ', [1, 2, 3].map(el => el + 1));
+const f = foo(2)
+const f2 = foo(5)
+
+
+console.log(f(2), f2(2))
