cacc8c46b3b9f9ab3baafa1a7f232231cacf92d8
Sergey <santerakis@gmail.com>
2023-05-20 19:48:42 +0300

    javascript => Javascript

--- src/test.js
+++ src/test.js
@@ -1,28 +1,33 @@
-var obj = { num: 2 };
+// javascript => Javascript
 
-function add(a, b){
-    return this.num + a + b;
+// С помощью метода toUpperCase() и slice():
+const word = 'javascript';
+const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+console.log(capitalizedWord); // Выведет: Javascript
+
+// С помощью метода replace() и регулярного выражения:
+const word = 'javascript';
+const capitalizedWord = word.replace(/^\w/, c => c.toUpperCase());
+console.log(capitalizedWord); // Выведет: Javascript
+
+// С помощью метода charAt() и конкатенации строк:
+const word = 'javascript';
+const capitalizedWord = word.charAt(0).toUpperCase() + word.substring(1);
+console.log(capitalizedWord); // Выведет: Javascript
+
+// Затем мы объединяем его с остальной частью слова, которую получаем с помощью slice(), substring() или replace()
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// "Hello world" => "heloWorld"
+const inputString = 'Hello world';
+const words = inputString.split(' ');
+
+// Преобразование каждого слова, кроме первого
+for (let i = 1; i < words.length; i++) {
+    const word = words[i];
+    const modifiedWord = word.charAt(0).toLowerCase() + word.slice(1);
+    words[i] = modifiedWord;
 }
 
-console.log(add.call(obj, 3, 5));
-
-////////////////////////////////////////////
-
-var obj = { num: 2 };
-
-function add(a, b){
-    return this.num + a + b;
-}
-
-console.log(add.apply(obj, [3, 5]));
-
-////////////////////////////////////////////
-
-var obj = { num: 2 };
-
-function add(a, b){
-    return this.num + a + b;
-}
-
-const func = add.bind(obj, 3, 5);
-func(); // Возвращает 10
+const outputString = words.join('');
+console.log(outputString);  // Выведет: heloWorld
